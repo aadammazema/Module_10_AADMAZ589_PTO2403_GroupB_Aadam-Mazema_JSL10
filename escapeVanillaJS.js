dd4411bbe728730cd33c3645fdff9efd7a4e28ad
Aadam Mazema <maze.aadam@gmail.com>
2024-09-13 19:21:56 +0200

    Fixed delay handling in navigateLabyrinth function
    Added await to ensure a 1-second delay is applied between steps in the navigateLabyrinth function.

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -13,14 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         ).textContent = `The key to the next room is: ${mostRecentBook.title}`;
       });
   });
-// Function to find the most recently published book based off date published
-  function findMostRecentBook(books) {
-    return books.reduce((mostRecent, book) => {
-      return new Date(book.published) > new Date(mostRecent.published)
-        ? book
-        : mostRecent;
-    });
-  }
+
 // Add click event listener to the element with ID 'solveRoom2'
   document.getElementById("solveRoom2").addEventListener("click", () => {
     const jsConcepts = new Set(["closure", "scope", "hoisting", "async"]);
@@ -35,30 +28,40 @@ document.addEventListener("DOMContentLoaded", () => {
     ).join(", ")}`;
   });
 
-  // 🪲 Bug: Asynchronous function ?
+ 
   document.getElementById("solveRoom3").addEventListener("click", () => {
     fetch("directions.json")
       .then((response) => response.json())
       .then((directions) => {
         navigateLabyrinth(directions).then((message) => {
-          // 🪲 Bug: Incorrect method
           document.getElementById("room3Result").innerHTML = message;
         });
       });
   });
 });
 
+// Function to find the most recently published book based off date published
+function findMostRecentBook(books) {
+    return books.reduce((mostRecent, book) => {
+      return new Date(book.published) > new Date(mostRecent.published)
+        ? book
+        : mostRecent;
+    });
+  }
 
 // Function to find intersection between JavaScript and React concepts
   function findIntersection(setA, setB) {
     return new Set([...setA].filter(item => setB.has(item)));
 }
-
+// Asynchronous function to navigate through a labyrinth based on given directions
 async function navigateLabyrinth(directions) {
+      // Loop through each direction in the directions array
   for (let direction of directions) {
-    // 🪲 Bug: No delay
-    new Promise((resolve) => setTimeout(resolve, 1000));
+    // 1 second delay added
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // Log the direction step to the console
     console.log(`Navigating: ${direction.step}`);
   }
+  // Return a completion message after all directions have been processed
   return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
 }
